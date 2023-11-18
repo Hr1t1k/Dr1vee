b176ff3297b4befe68c61329e98c3e607efc9eb9
Hr1t1k <hritikkaushik@gmail.com>
2023-11-18 21:26:30 +0530

    updated readme

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-# React + Vite
+#Dr1vee
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Google drive clone
 
-Currently, two official plugins are available:
+Link: dr1vee.web.app
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+To run locally :
+Clone the repo into your local drive
+npm i
+npm run dev (use -- --host to access from different devices on same network)
--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import HeaderDrive from "./components/HeaderDrive";
 import Sidebar from "./components/Sidebar";
 import "./App.css";
 import Content from "./components/Content";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { LayoutProvider } from "./context/LayoutContext";
 function App() {
+  const navigate = useNavigate();
+  const location = useLocation();
   const [grid, setGrid] = useState(
     window.localStorage.getItem("grid") === "false" ? false : true
   );
-
+  useEffect(() => {
+    if (location.pathname == "/") navigate("/my-drive");
+  });
   return (
     <div className="p-0 body">
       <HeaderDrive />
